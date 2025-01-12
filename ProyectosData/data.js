92e0d9b041bbbc15a780b627ff0bd5ad7fdf3305
LeandroPenna1 <leandro4penna@gmail.com>
2025-01-12 16:48:23 -0300

    correcciones

--- ProyectosData/data.js
+++ ProyectosData/data.js
@@ -3,3 +3,25 @@ function setModalImage(src) {
 }
 
 let goles = "/img/Data/Goles.png"
+/*  */
+function seleccionar(link){
+    var opciones = document.querySelector('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+/* Menu responsive */
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if(x.className==="") {
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
